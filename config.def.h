aa3c49df98e57f70ef31e821b3835b4913b497b4
GtlsGamr <hittarth91@gmail.com>
2021-08-15 14:12:16 +0530

    added_

--- config.def.h
+++ config.def.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrainsMono Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"IBM Plex Sans Devanagari:style=Regular",
+};
 static int borderpx = 10;
 
 /*
